879083222fd3e07d1789789d3746f86bd3af90ba
Lucio Romero Mascaró <lromerom@fi.uba.ar>
2026-09-13 13:43:21 -0300

    resolucion1 del ej1.rpl

--- rpl/3_backtracking/ej1.py
+++ rpl/3_backtracking/ej1.py
@@ -21,7 +21,7 @@ str
 """
 """
 planteo:
-parecido al coloreo de grafos, no?
+parecido al coloreo de grafos, no? o IS¿?
 
 me tengo que curtir con backtracking. es importante. lleva mas codigo que dificultad conceptual.
 
@@ -41,6 +41,31 @@ Backtracking es siempre recursivo?
 
 """
 def no_adyacentes(grafo, n):
-    'Devolver una lista con los n vértices, o None de no ser posible'
+    #'Devolver una lista con los n vértices, o None de no ser posible'
+    if n > len(grafo.obtener_vertices()):
+        return [] # n no puede ser mayor  a la cantidad de vértices.
+    vert_no_ady = []
+    visitados = set()
+    
+    for v in grafo.obtener_vertices(): # tengo que hacerlo si o si para admitir más de una comp. conexa, o no?
+        todo_ok = True
+        if v not in visitados:
+            for ady in grafo.obtener_adyacentes(v):
+                if ady not in visitados:
+                    visitados.add(ady)
+                else:
+                    if ady in vert_no_ady:
+                        todo_ok = not todo_ok
+            if todo_ok:
+                vert_no_ady.append(v)
+
+    return vert_no_ady
+
+
+
+
+
+
+
     pass
 
