8b27306adef0208e01500f1bc5e8aa4de6c36a91
Lucio Romero Mascaró <lromerom@fi.uba.ar>
2026-09-25 21:55:40 -0300

    ej1 pd rpl. todo ok.

--- rpl/4_pd/ej1.py
+++ rpl/4_pd/ej1.py
@@ -1,12 +1,54 @@
 """
 1. Ej.1 (★):
-    Implementar un algoritmo que, utilizando programación dinámica, obtenga el valor del n-ésimo número de fibonacci. Indicar y justificar la complejidad del algoritmo implementado. Definición:
-        n = 0 --> Debe devolver 1
-        n = 1 --> Debe devolver 1
-        n --> Debe devolver la suma entre los dos anteriores números de fibonacci (los fibonacci n-2 y n-1)
+Implementar un algoritmo que, utilizando programación dinámica, obtenga el valor del n-ésimo número de fibonacci. 
+Indicar y justificar la complejidad del algoritmo implementado. 
 
+Definición:
+. n = 0 --> Debe devolver 1
+. n = 1 --> Debe devolver 1
+. n --> Debe devolver la suma entre los dos anteriores números de fibonacci (los fibonacci n-2 y n-1)
+"""
+
+"""
+planteo:
+
+primero pienso en la ecuación de recurrencia --> forma de los subproblemas
+
+sé los casos bases y sé el patrón que sigue.
+
+T(N) = OPT(N-2) + OPT(N-1)
+
+utilizo un for para llenar un arreglo de memoria (tecnica memoization).
+
+después de eso, sería averiguar el fibonacci de N
+que es la parte de reconstrucción.
+
+igual creo que no hace falta. es hacer N iteraciones. y no utilizo un arreglo para recordar, solo las dos últimas
 
 """
 
 def fibonacci(n):
-    return 1
+    if n <= 1:
+        return 1
+
+    actual = 1
+    anterior = 0
+    total = 0
+    for i in range(n):
+        total = actual + anterior
+        anterior = actual
+        actual = total
+        i += 1
+
+    return total
+
+
+"""
+Justificación de la complejidad:
+
+- temporal: O(n), siendo n la cantidad de iteraciones necesarias hasta llegar al n-ésimo valor de la secuencia de fibonacci.
+- espacial: O(1), utilizando variables se consigue una complejidad espacial constante.
+
+
+"""
+
